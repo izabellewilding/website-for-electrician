d9894d9384dc97e6f0b61e3ab4cbccb463012fb7
IzzieWilding <izabellewilding@gmail.com>
2019-10-26 16:05:04 +0100

    fixed problem with svg black border

--- src/components/header.js
+++ src/components/header.js
@@ -6,31 +6,39 @@ import classNames from "classnames"
 import "@rmwc/icon/icon.css"
 import { Icon } from "@rmwc/icon"
 import Appliance from "../assets/applicance.svg"
+import Bulb from "../assets/light-bulb.svg"
+import Rewire from "../assets/plugs.svg"
 
 const Header = () => {
   const [open, setOpen] = useState(false)
   return (
-    <header className="header">
-      <Icon
-        icon="keyboard_arrow_down"
-        className={classNames("arrow-down", { "arrow-down-off": open })}
-        onClick={() => setOpen(true)}
-      />
-      <Icon
-        icon="keyboard_arrow_up"
-        className={classNames("arrow-up-off", { "arrow-up": open })}
-        onClick={() => setOpen(false)}
-      />
+    <>
+      <header className="header">
+        <Icon
+          icon="keyboard_arrow_down"
+          className={classNames("arrow-down", { "arrow-down-off": open })}
+          onClick={() => setOpen(true)}
+        />
+        <Icon
+          icon="keyboard_arrow_up"
+          className={classNames("arrow-up-off", { "arrow-up": open })}
+          onClick={() => setOpen(false)}
+        />
+
+        {/* end header dropdown */}
+      </header>
 
       <div
         className={classNames("header-dropdown", {
           "header-dropdown-active": open,
         })}
       >
-        <Appliance className="applicance-svg" />
+        <h1 className="services-title">OUR SERVICES</h1>
+        <Appliance className="appliance-svg" />
+        <Bulb className="light-bulb-svg" />
+        <Rewire className="rewire-svg" />
       </div>
-      {/* end header dropdown */}
-    </header>
+    </>
   )
 }
 
