155f6948e62572ca8e82f8687d4013c62dbe6996
IzzieWilding <izabellewilding@gmail.com>
2019-11-03 09:47:54 +0000

    index font size change

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,17 +21,19 @@ const IndexPage = () => {
           className="index-img"
         />
         <div className="homepage-wrapper">
-          <div className="website-logo">
-            <p className="main-heading">Alex Wilding</p>
-            <p className="sub-heading">Electrical Services</p>{" "}
-          </div>
-          <div className="website-description">
-            <h1>
-              Commercial and domestic electrician with over 10 years experience.
-              No job too big or small.
-            </h1>
-            <br />
-            <h2>Get in contact for a quote.</h2>
+          <div className="text-wrapper">
+            <div className="website-logo">
+              <p className="main-heading">Alex Wilding</p>
+              <p className="sub-heading">Electrical Services</p>{" "}
+            </div>
+            <div className="website-description">
+              <h1>
+                Commercial and domestic electrician with over 10 years
+                experience. No job too big or small.
+              </h1>
+              <br />
+              <h2>Get in contact for a quote.</h2>
+            </div>
           </div>
         </div>
         {/* <div className="contact-details">
