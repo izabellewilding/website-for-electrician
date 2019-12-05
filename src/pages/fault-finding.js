59f8fda44961d9f6da3f75c87a149a1896a6113e
IzzieWilding <izabellewilding@gmail.com>
2019-12-05 20:51:46 +0000

    removed lorem ipsum from fault finding pg

--- src/pages/fault-finding.js
+++ src/pages/fault-finding.js
@@ -22,23 +22,6 @@ const ServiceArticle = () => (
         <div className="article-text">
           {" "}
           <h1 className="article-subheading">FAULT FINDING</h1>
-          <p>
-            {" "}
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur.
-          </p>
-          <br />
-          <p>
-            {" "}
-            Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
-          </p>
           <a
             href="mailto:alex@alexwildingelectricalservices.com"
             className="mailto-link mailto-services"
