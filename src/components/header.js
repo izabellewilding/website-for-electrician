d6165b7dd9d1928d5f03a10cf6d1118bc3b3399e
IzzieWilding <izabellewilding@gmail.com>
2020-01-10 20:11:16 +0000

    max-width on top bar

--- src/components/header.js
+++ src/components/header.js
@@ -46,8 +46,10 @@ const Header = props => {
   return (
     <>
       <div className="top-section">
-        <p>Electrician in Pembrokeshire </p> <Clock className="clock-svg" />
-        <p>08:00 - 21:00 Mon - Sat, 09:00 - 17:00 Sun</p>
+        <div className="top-section-content">
+          <p>Electrician in Pembrokeshire </p> <Clock className="clock-svg" />
+          <p>08:00 - 21:00 Mon - Sat, 09:00 - 17:00 Sun</p>
+        </div>
       </div>
       <header className="header">
         <div className="header-content">
